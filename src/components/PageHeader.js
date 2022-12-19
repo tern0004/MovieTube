0f381ab35f5593edf40265fdde9d9cdafb615d03
Pavlo <p.tern2298@gmail.com>
2022-12-19 18:50:12 -0500

    - Added Page Header styles; - Edited Page Header component;

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import '../style/Header.css';
 export default function PageHeader() {
     return(
-        <div>
-            <h2>Page header</h2>
+        <div className='header-container'>
+            <div className='header-content'>
+                <img className='header-logo' src='' alt=''/>
+                <img className='header-avatar' src='https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Free-Download.png' alt='login'/>
+            </div>
+
         </div>
     )
 }
